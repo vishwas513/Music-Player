6544bf1e78ed2bf526702518ee44183d1911980f
Vishwas Mukund <vishwas.mukund@sjsu.edu>
2017-05-22 22:11:57 -0700

    First version

--- Bach/ViewController.swift
+++ Bach/ViewController.swift
@@ -7,12 +7,105 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    var player = AVAudioPlayer()
+    let audiopath = Bundle.main.path(forResource : "sheep", ofType : "mp3")
+    var timer = Timer()
+
+    
+    var status = false
+    func updateScrubber() {
+        
+        cue.value = Float(player.currentTime)
+        
+    }
+    
+    
+    
+    @IBOutlet weak var playbutton: UIBarButtonItem!
+    @IBAction func RW(_ sender: Any) {
+    player.currentTime = player.currentTime - TimeInterval(10)
+    
+    
+    }
+    @IBAction func FF(_ sender: Any) {
+    
+    player.currentTime = player.currentTime + TimeInterval(10)
+    
+    }
+    @IBAction func play(_ sender: Any) {
+
+        if status == false {
+        
+        
+            do{ player.play()}
+            catch{print(Error.self)}
+            
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateScrubber), userInfo: nil, repeats: true)
+            status = true
+            
+        
+        
+        }
+        
+        else if status == true {
+        
+        
+        player.pause()
+        timer.invalidate()
+        status = false
+        
+        }
+        
+        
+        
+    
+    
+    }
+    @IBOutlet weak var vol: UISlider!
+    @IBAction func volchanged(_ sender: Any) {
+    
+        player.volume = vol.value
+        
+    }
+    
+    
+    @IBOutlet weak var cue: UISlider!
+    
+    @IBAction func cueAction(_ sender: Any) {
+    
+    
+        player.currentTime = TimeInterval(cue.value)
+    
+    }
+    
+    
+   
+    @IBOutlet weak var songname: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audiopath!))
+            
+            cue.maximumValue = Float(player.duration)
+            
+            
+            
+        } catch {
+            
+            // process error
+            
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
